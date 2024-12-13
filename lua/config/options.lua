d7c2e4a0600542b5dd617d5b19ea52a9f3bf04aa
n0rrman <mail@henriknorrman.com>
2024-12-14 00:30:51 +0100

    diagnostic plugin added

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,6 +5,12 @@ vim.opt.fixeol = false          -- Equivalent to 'nofixeol'
 vim.opt.swapfile = false
 vim.opt.undofile = true
 vim.opt.undodir = vim.fn.stdpath("data") .. "/undo"
+vim.diagnostic.config({
+	virtual_text = false,
+	signs = true,
+	underline = true,
+	severity_sort = true,
+})
 
 
 -- INDENTATION
