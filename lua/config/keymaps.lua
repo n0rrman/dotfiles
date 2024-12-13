d6e25d1d1b7e2a7046d345b5851bd704ad397b3f
n0rrman <mail@henriknorrman.com>
2024-12-13 22:54:44 +0100

    fix

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -53,7 +53,7 @@ vim.keymap.set("n", "<leader>ak", "<cmd>lua vim.diagnostic.goto_prev()<cr>", opt
 
 -- Telescope
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", builtin.find_files, { opts, desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
 vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
